3e2c0465383310a6d02bd1be720bf6d498772ca2
CarolPera <carol_p.100@hotmail.com>
2025-01-26 10:38:30 -0300

    initial commit

---
+++ src/main/kotlin/hackaton/processor/ProcessorApplication.kt
@@ -0,0 +1,11 @@
+package hackaton.processor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProcessorApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProcessorApplication>(*args)
+}
